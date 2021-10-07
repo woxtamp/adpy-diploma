579ba00eebf279d51b9305d9815c7a0d319a4836
Sergey <earnedgravy@gmail.com>
2021-10-07 19:36:39 +0700

    polish to perfection

--- db_creation/db_creation_scripts.sql
+++ db_creation/db_creation_scripts.sql
@@ -1,60 +1,32 @@
-create table if not exists vk_user (
-	id serial primary key,
-	vk_id integer not null unique,
-	age integer not null,
-	sex integer not null,
-	city_name varchar(250) not null,
-	city_id integer
-);
-
-create table if not exists vk_search_params (
-	id serial primary key,
-	vk_id integer not null unique,
-	age_from integer not null,
-	age_to integer not null,
-	sex integer not null,
-	city_name varchar(250) not null,
-	city_id integer
-);
-
-create table if not exists vk_find_user (
-	id serial primary key,
-	vk_id integer not null,
-	vk_search_id integer not null,
-	constraint vk_id_vk_search_id_vk_find_user unique (vk_id, vk_search_id)
-);
-
-добавить связи между таблицами
-
-create table if not exists vk_user (
-	id serial primary key,
-	vk_id integer not null unique,
-	age integer not null,
-	sex integer not null,
-	city_name varchar(250) not null,
-	city_id integer
-);
-
-create table if not exists vk_search_params (
-	id serial primary key,
-	vk_id integer not null unique,
-	age_from integer not null,
-	age_to integer not null,
-	sex integer not null,
-	city_name varchar(250) not null,
-	city_id integer
-);
-
-create table if not exists vk_find_user (
-	id serial primary key,
-	vk_id integer not null,
-	vk_search_id integer not null,
-	constraint vk_id_vk_search_id_vk_find_user unique (vk_id, vk_search_id)
-);
-
 create table if not exists vk_user_token (
 	id serial primary key,
-	vk_id integer not null,
+	vk_id integer not null unique,
 	vk_user_token varchar(1000) not null,
     vk_token_lifetime integer not null
+);
+
+create table if not exists vk_user (
+	id serial primary key,
+	vk_id integer not null unique references vk_user_token(vk_id),
+	age integer not null,
+	sex integer not null,
+	city_name varchar(250) not null,
+	city_id integer
+);
+
+create table if not exists vk_search_params (
+	id serial primary key,
+	vk_id integer not null unique references vk_user(vk_id),
+	age_from integer not null,
+	age_to integer not null,
+	sex integer not null,
+	city_name varchar(250) not null,
+	city_id integer
+);
+
+create table if not exists vk_find_user (
+	id serial primary key,
+	vk_id integer not null references vk_search_params(vk_id),
+	vk_search_id integer not null,
+	constraint vk_id_vk_search_id_vk_find_user unique (vk_id, vk_search_id)
 );
